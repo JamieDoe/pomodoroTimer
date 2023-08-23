bd6a33845e3a6abb77f547390b469d760200a086
JamieDoe <up953068@myport.ac.uk>
2023-08-23 14:10:53 +0100

    added content for pomodoro

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import { useState } from 'react'
 export default function App() {
 
   return (
+
     <>
-      This is a Pomodoro App built with react.
+      <h1>PomodoroJS</h1>
+      <div>
+        <h2>Work Session</h2>
+        <p>00:00</p>
+      </div>
+      <div>
+        <h2>Break Session</h2>
+        <p>00:00</p>
+      </div>
+      
     </>
   )
 }
